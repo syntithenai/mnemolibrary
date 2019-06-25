231a980f4d34f32ab4025e3ec14ab9ad809f97f1
steve <stever@syntithenai.com>
2019-06-25 18:55:02 +1000

    wip

--- oauth/database/index.js
+++ oauth/database/index.js
@@ -13,15 +13,15 @@ function connect(connectString='') {
   if (connectString.length == 0) {
       connectString = config.databaseConnection + config.database;
   }
-  try {
-	  mongoose.connect(connectString,{useMongoClient: true}).then(() => {
-		console.log('Mongoose Connected');
-	  }).catch((err) => {
-		console.log(err);
-	  });
-	} catch (e) {
-		console.log(e);
-	}
+  //try {
+	  //mongoose.connect(connectString,{useMongoClient: true}).then(() => {
+		//console.log('Mongoose Connected');
+	  //}).catch((err) => {
+		//console.log(err);
+	  //});
+	//} catch (e) {
+		//console.log(e);
+	//}
 }
 
 module.exports = {
